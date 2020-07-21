3a0b5211cbb2f1517db9fd91a5e3c403c888f1a3
achintya219 <goyal.achint1234@gmail.com>
2020-07-21 21:18:33 +0530

    ..

--- Python/ques4.py
+++ Python/ques4.py
@@ -1,1 +1,7 @@
 import numpy as np 
+
+X = np.random.normal(0, 1, (20, 20))
+y = np.random.randint(2, size=(20,1), dtype='int32')
+
+theta = np.dot(np.dot(np.linalg.pinv(np.dot(X.T, X)), X.T), y)
+print(theta)
