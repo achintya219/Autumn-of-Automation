b7ebc23a85b01d087d0bf1f68b7becd8066185ac
achintya219 <goyal.achint1234@gmail.com>
2020-07-24 21:12:00 +0530

    Update 1.3

--- OpenCV/asgn1-3.py
+++ OpenCV/asgn1-3.py
@@ -2,11 +2,7 @@ import cv2 as cv
 
 nemo = cv.imread("/home/achintya/Desktop/Autumn-of-Automation/OpenCV/nemo3.jpg")
 
-lower = (150, 0, 0)
-upper = (255, 150, 50)
-
-mask = cv.inRange(nemo, lower, upper)
-nemo[mask != 0] = [0, 0, 255]
+nemo = cv.cvtColor(nemo, cv.COLOR_BGR2RGB)
 
 cv.imshow("Display Window", nemo)
 cv.waitKey(0)
