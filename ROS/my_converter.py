125538f2b0035d82bf1d533333d4b878e8a5d0be
achintya219 <goyal.achint1234@gmail.com>
2020-07-31 20:39:06 +0530

    ROS basic assignment

--- ROS/my_converter.py
+++ ROS/my_converter.py
@@ -3,48 +3,34 @@
 
 import rospy
 import numpy as np
-from std_msgs.msg import String
-from std_msgs.msg import Float64 
+from beginner_tutorials.msg import quat
+from beginner_tutorials.msg import rollpitchyaw 
 
-roll=0
-pitch=0
-yaw=0
+rpy = rollpitchyaw
 
 def callback(data):
-	sinr_cosp = 2*(data.data[0] * data.data[1] + data.data[2] * data.data[3])
-	cosr_cosp = 1-2*(data.data[1]**2 + data.data[2]**2)
-	roll = np.arctan2(sinr_cosp, cosr_cosp)
+	val = data
+	sinr_cosp = 2*(val.x * val.y + val.z * val.w)
+	cosr_cosp = 1-2*(val.y**2 + val.z**2)
+	rpy.roll = np.arctan2(sinr_cosp, cosr_cosp)
 
-	sinp = 2 * (data.data[0] * data.data[2] - data.data[3] * data.data[1])
-	pitch = np.where(np.abs(sinp) >= 1,
+	sinp = 2 * (val.x * val.z - val.w * val.y)
+	rpy.pitch = np.where(np.abs(sinp) >= 1,
 					np.sign(sinp) * np.pi / 2,
 					np.arcsin(sinp))
 
-	siny_cosp = 2 * (data.data[0] * data.data[3] + data.data[1] * data.data[2])
-	cosy_cosp = 1 - 2 * (data.data[2]**2 + data.data[3]**2)
-	yaw = np.arctan2(siny_cosp, cosy_cosp)
+	siny_cosp = 2 * (val.x * val.w + val.y * val.z)
+	cosy_cosp = 1 - 2 * (val.z**2 + val.w**2)
+	rpy.yaw = np.arctan2(siny_cosp, cosy_cosp)
 
-def nav():
-	rospy.init_node('Converter', anonymous=True)
+
+	pub = rospy.Publisher('topic2', rollpitchyaw, queue_size=10)
+		
+	pub.Publish(rpy)
 	
-	rospy.Subscriber("topic1", Float64, callback)
-
-	rospy.spin()
-
-	pub = rospy.Publisher('topic2', Float64, queue_size=10)
-	rate = rospy.Rate(10)
-
-	while not rospy.is_shutdown():
-		euler_angles = [roll, pitch, yaw]
-		rospy.loginfo(euler_angles)
-		pub.Publish(euler_angles)
-		rate.sleep()
 
 
-if __name__ == '__main__':
-    try:
-        nav()
-    except rospy.ROSInterruptException:
-        pass
+rospy.init_node('Converter', anonymous=True)
 	
+rospy.Subscriber('topic1', quat, callback)
 
